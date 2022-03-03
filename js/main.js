60091b234d131bdd35de0738ff2ba1bb2142c541
Marcelino A. Abreu Mejia <marcelinomejia2009@gmail.com>
2022-03-03 13:29:00 -0400

    Desktop finished

--- js/main.js
+++ js/main.js
@@ -3,3 +3,18 @@ $(function(){
         dateFormat: "dd.mm.yy"
     });
 });
+
+const carousel = new Glide('.glide', {
+    type: 'carousel',
+    perView: 3,
+    gap: 20,
+    focusAt: 'center',
+    autoplay: 7000,
+    animationDuration: 1500,
+    peek: {
+        before: 150,
+        after: 150
+    }
+});
+
+carousel.mount();
